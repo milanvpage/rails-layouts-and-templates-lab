07b302f6cc237c9ffc090d96c4b0bb550315914b
Milan <milanvpage@gmail.com>
2021-07-20 11:00:10 -0700

    commit tree 7dfb7ca28284e7b78bb23588507c422b1c0020fd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
